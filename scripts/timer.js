a8dd316d0197b99393ddd6659019c193e1c4d499
shtabyl <avnirog@gmail.com>
2025-05-25 16:13:12 +0300

    Feat: add timer
    Countdown starts after click on a board cell and stops
    after returning to the board.

---
+++ scripts/timer.js
@@ -0,0 +1,66 @@
+function CountdownController(initialTime) {
+    let timerId;
+
+    const screen = CountdownScreenController();
+
+    const startCountdown = () => {
+        loopCountdown(initialTime);
+    }
+
+    const loopCountdown = (time) => {
+        console.log(time);
+        time--;
+        if (time < 0) {
+            return;
+        }
+        screen.updateScreen(time);
+        timerId = setTimeout(() => loopCountdown(time), 1000);
+    }
+
+    const stopCountdown = () => {
+        clearTimeout(timerId);
+    }
+
+    return { startCountdown, stopCountdown };
+}
+
+// Screen performance
+function CountdownScreenController() {
+    const countdownBox = document.querySelector('.countdown-box');
+    
+    const updateScreen = (time) => {
+        countdownBox.textContent = ('0' + time).slice(-2);
+    }
+    
+    return { updateScreen }
+}
+
+function TimerController() {
+    let initialTime = 15;
+    
+    const timer = CountdownController(initialTime);
+    const screen = CountdownScreenController();
+    
+    const timerBox = document.querySelector('.timer-box');
+    const boardCells = document.querySelectorAll('.cell_link');
+    const currentQuestionBox = document.querySelector('.current-question-box');
+
+    boardCells.forEach((cell) => {
+        cell.addEventListener('click', (e) => {
+            screen.updateScreen(initialTime);
+            timerBox.style.color = 'white';
+            setTimeout(() => timer.startCountdown(initialTime), 1000);
+        }); 
+    });
+
+    currentQuestionBox.addEventListener('click', (e) => {
+        screen.updateScreen(initialTime);
+        timerBox.style.color = '#a0c3ff';
+        timer.stopCountdown();
+    });
+
+    // Initial screen
+    screen.updateScreen(initialTime);
+}
+
+TimerController();
